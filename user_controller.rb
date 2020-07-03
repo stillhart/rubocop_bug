912f0ba74bbd25c169101a2497c2e719a4bf1d90
Fabian Stillhart <fabian.stillhart1@swisscom.com>
2020-07-03 17:03:59 +0200

    trim down example

--- user_controller.rb
+++ user_controller.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def update
-    if current_user.superuser? || current_user.admin?
-      super_ = !!ActiveRecord::Type::Boolean.new.cast(params[:superuser])
-      User.find(params[:id]).update(superuser: super_)
-    end
-  end
-
   private
 end
